677dd70252a4ecbd89fbbcc0bb773f3ade91407f
WSP International <WSP International@DESKTOP-V2V28IJ>
2024-07-28 20:32:54 -0600

    add another row

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World! nice day");
+            Console.WriteLine("another row");
             Console.ReadLine();
         }
     }
