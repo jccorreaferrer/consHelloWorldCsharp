45b8315eac200d5accf3b6a1b40d0073b726c6e1
WSP International <WSP International@DESKTOP-V2V28IJ>
2024-07-28 20:20:37 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace consHelloWorldCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadLine();
+        }
+    }
+}
